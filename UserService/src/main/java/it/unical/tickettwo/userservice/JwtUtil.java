d4f58f69d99a2167e2775501d79ecd5f60b32f35
Mxtr-j <156937301+Mxtr-j@users.noreply.github.com>
2025-05-22 11:07:31 +0200

    pubblicazione post fixata e si mostrano i top3 nella home

--- UserService/src/main/java/it/unical/tickettwo/userservice/JwtUtil.java
+++ UserService/src/main/java/it/unical/tickettwo/userservice/JwtUtil.java
@@ -8,15 +8,17 @@ import java.util.Date;
 
 public class JwtUtil {
 
-    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    private static final long EXPIRATION_TIME = 86400000; // 1 giorno in millisecondi
+    private static final String SECRET_KEY = "my-super-secret-key-which-should-be-long-enough";
+    private static final long EXPIRATION_TIME = 86400000; // 1 giorno
+
+    private static final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
 
     public static String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(key)
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
 
@@ -30,7 +32,11 @@ public class JwtUtil {
     }
 
     public static boolean validateToken(String token) {
-        Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-        return true;
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
     }
 }
--- UserService/src/main/java/it/unical/tickettwo/userservice/domain/UsersAccounts.java
+++ UserService/src/main/java/it/unical/tickettwo/userservice/domain/UsersAccounts.java
@@ -22,4 +22,44 @@ public class UsersAccounts {
     private String role;
     @Column(name="access_type", nullable = false)
     private String accessType;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getAccessType() {
+        return accessType;
+    }
+
+    public void setAccessType(String accessType) {
+        this.accessType = accessType;
+    }
 }
